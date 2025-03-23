8da598d28f9e5c432f45f470eb5ed974e460c0b8
ohtsuka-shota <ohtsuka.honban@gmail.com>
2025-03-23 17:15:17 +0900

    PWA プラグイン導入

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'AWS-Quiz-App',
+        short_name: 'AWS-Quiz-App',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#3f51b5',
+        icons: [
+          {
+            src: 'PWA-icon/aws-icon.svg',
+            sizes: '256x256',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+});
